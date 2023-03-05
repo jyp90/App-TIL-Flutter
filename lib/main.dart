6543f3bccf6ee414b55c445ba79395635baab5f0
jyp90 <jypark@estatecloudinc.com>
2023-03-05 16:01:13 +0900

    feat: elevation, mainAxisAlignment, Center 클래스 설명

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Mungmoong App",
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: MyHomePage(),
+    return const MaterialApp(
+      title: "Charactor card",
+      home: MyCard(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyCard extends StatelessWidget {
+  const MyCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Introduce vely!')
+        title: const Text('Vely'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+        elevation: 0.0,
       ),
-      body: Center(
+      body: Center( // 화면 정중앙에 보이도록 함
         child: Column(
-          children: <Widget>[
-            Text("Hello my pretty dog"),
-            Text("His name is vely"),
-            Text("He wear white furs")
-          ]
-        )
+          mainAxisAlignment: MainAxisAlignment.center, // 상단 중간 하단 정렬 관련
+          children: const <Widget>[
+            Text("Hello"),
+            Text("Hello"),
+            Text("Hello"),
+          ],
+        ),
       )
     );
   }
