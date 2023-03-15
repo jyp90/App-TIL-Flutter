21fbcb29e5b8a550ea353290ebb5b894bcfc0596
jyp90 <jypark@estatecloudinc.com>
2023-03-15 23:01:38 +0900

    feat: 폴딩된 내용중에 ListTile 을 사용하여 아이템 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,78 @@ class MyPage extends StatelessWidget {
           }),
         ],
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/vely.png'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                    'assets/roopy.png'
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage(
+                //       'assets/roopy.png'
+                //   ),
+                //   backgroundColor: Colors.white,
+                // ),
+              ],
+              accountName: Text('vely'),
+              accountEmail: Text('jyp@kakao.com'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[850]),
+              title: Text(
+                'Home'
+              ),
+              onTap: (){
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                  color: Colors.grey[850]),
+              title: Text(
+                  'Setting'
+              ),
+              onTap: (){
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                  color: Colors.grey[850]),
+              title: Text(
+                  'Q&A'
+              ),
+              onTap: (){
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
+            )
+          ]
+        )
+      ),
     );
   }
 
