6365590f32e76e2dc6de8b5c83dceeba8c4e8e4d
jyp90 <jypark@estatecloudinc.com>
2023-03-15 21:17:44 +0900

    feat: 상단바 버튼 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,141 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-const characterName = "맹수쪼꼬";
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget{
+
+
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Character card",
-      home: Grade(),
+    return MaterialApp(
+        title: 'AppBar',
+        theme: ThemeData(
+            primarySwatch: Colors.red
+        ),
+      home: MyPage(),
     );
+
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({Key? key}) : super(key: key);
-
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[300],
       appBar: AppBar(
-        title: const Text(characterName),
-        backgroundColor: Colors.deepPurple[400],
+        title: Text('Appbard icon '),
         centerTitle: true,
         elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬을 맞춰주는 속성
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/choco2.jpeg"),
-                radius: 60.0,
-              ),
-            ),
-            Divider(
-              height: 60.0, // Divider 의 위 30, 아래 30 떨어진 의미
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            const Text("NAME",
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            ) // 텍스트 스타일 추가
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(characterName,
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-            const SizedBox(
-              height: 30.0
-            ),
-            const Text("$characterName 레벨과 특징",
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                ) // 텍스트 스타일 추가
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text("93",
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("콧물을 잘흘리는 애교왕",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                )),
-              ],
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("하얀 수염의 맹수",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0,
-                    )),
-              ],
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("식탐이 강함",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0,
-                    )),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: const AssetImage("assets/choco-sleep.jpeg"),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
-              )
-            )
-          ],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('menu button clicked ');
+          },
         ),
-      )
+        actions: <Widget>[
+          IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {
+            print('shopping_cart button clicked ');
+          }
+        ),
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            print('search button clicked ');
+          }),
+        ],
+      ),
     );
   }
+
 }
 
