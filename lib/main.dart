e71703d9e0860cf356dbccb7d9bf19bfef4c369f
jyp90 <jypark@estatecloudinc.com>
2023-03-18 11:07:03 +0900

    feat: 스낵바 아이템을 구현하고 Builder 써보기

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'AppBar',
-        theme: ThemeData(
-            primarySwatch: Colors.red
-        ),
+      title: 'AppBar',
+      theme: ThemeData(
+          primarySwatch: Colors.red
+      ),
       home: MyPage(),
     );
-
   }
 }
 
@@ -23,104 +21,30 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Appbard icon '),
-        centerTitle: true,
-        elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button clicked ');
-          },
+        appBar: AppBar(
+          title: Text('Snack Bar'),
+          centerTitle: true,
         ),
-        actions: <Widget>[
-          IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {
-            print('shopping_cart button clicked ');
-          }
-        ),
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {
-            print('search button clicked ');
-          }),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/vely.png'),
-                backgroundColor: Colors.white,
-              ),
-              otherAccountsPictures: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'assets/roopy.png'
-                  ),
-                  backgroundColor: Colors.white,
-                ),
-                // CircleAvatar(
-                //   backgroundImage: AssetImage(
-                //       'assets/roopy.png'
-                //   ),
-                //   backgroundColor: Colors.white,
-                // ),
-              ],
-              accountName: Text('vely'),
-              accountEmail: Text('jyp@kakao.com'),
-              onDetailsPressed: () {
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),
-                  bottomRight: Radius.circular(40.0),
+        body: Builder(builder: (BuildContext ctx) {
+          return
+            Center(
+                child: FlatButton(
+                    child: Text('Show me',
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                    color: Colors.red,
+                    onPressed: () {
+                      Scaffold.of(ctx).showSnackBar(
+                          SnackBar(content: Text('Hello'),
+                          ))
+                    }
                 )
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home,
-              color: Colors.grey[850]),
-              title: Text(
-                'Home'
-              ),
-              onTap: (){
-                print('Home is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings,
-                  color: Colors.grey[850]),
-              title: Text(
-                  'Setting'
-              ),
-              onTap: (){
-                print('Home is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.question_answer,
-                  color: Colors.grey[850]),
-              title: Text(
-                  'Q&A'
-              ),
-              onTap: (){
-                print('Q&A is clicked');
-              },
-              trailing: Icon(Icons.add),
             )
-          ]
+        }
         )
-      ),
     );
   }
 
 }
-
