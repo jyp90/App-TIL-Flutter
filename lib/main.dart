9830007f1649c154d2f9e8fbc99bd948c2c32789
jyp90 <jypark@estatecloudinc.com>
2023-03-18 11:22:20 +0900

    feat: SnackBar & Toast Message 띄워보기

--- lib/main.dart
+++ lib/main.dart
@@ -21,30 +21,39 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Snack Bar'),
-          centerTitle: true,
-        ),
-        body: Builder(builder: (BuildContext ctx) {
-          return
-            Center(
-                child: FlatButton(
-                    child: Text('Show me',
-                      style: TextStyle(
-                          color: Colors.white
-                      ),
-                    ),
-                    color: Colors.red,
-                    onPressed: () {
-                      Scaffold.of(ctx).showSnackBar(
-                          SnackBar(content: Text('Hello'),
-                          ))
-                    }
-                )
-            )
-        }
-        )
+      appBar: AppBar(
+        title: const Text('Snack Bar'),
+        centerTitle: true,
+      ),
+      body: const MySnackBar(),
     );
   }
 
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: MaterialButton(
+          child: const Text('Show me'),
+          onPressed: () {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(
+                content: Text('Hello',
+                textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                ),
+              backgroundColor: Colors.teal,
+                duration: Duration(milliseconds: 1000),
+            )
+            );
+          }
+      ),
+    );
+  }
 }
