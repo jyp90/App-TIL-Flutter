0caa5a1e8e0529e1b103c35c9281d228b5c93974
jyp90 <jypark@estatecloudinc.com>
2023-03-19 11:25:11 +0900

    feat: TextButton, ElevatedButton, OutlinedButton 버튼 속성과 꾸미기 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,70 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AppBar',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: MyPage(),
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: const MyButtons(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyPage extends StatelessWidget {
+class MyButtons extends StatelessWidget {
+  const MyButtons({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Container(
-          color: Colors.red,
-          width: 100,
-          height: 100,
-          margin: EdgeInsets.symmetric(
-            vertical: 80,
-            horizontal: 20
-          ),
-          padding: EdgeInsets.all(40),
-          child: Text('Hello'),
+        appBar: AppBar(
+          title: Text("Button"),
+          centerTitle: true,
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  print('text button');
+                },
+                child: Text(
+                  'Text button',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('Elevated button');
+                  },
+                  child: Text('Elevated Button'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orangeAccent, // 버튼 색깔 옵션
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0), // 라운드 둥글게 옵션
+                ),
+                elevation: 0.0, // 그림자 제거 옵션
+              ),
+              ),
+              OutlinedButton(onPressed: () {
+                print('Outlined Button');
+              }, child: Text('Outlined button'),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.black87,
+                side: BorderSide(
+                  color: Colors.black87,
+                  width: 2.0,
+                ),
+              )),
+            ],
+          ),
+        ));
   }
 }
