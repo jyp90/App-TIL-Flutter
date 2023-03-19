81fca871f8939b9ffafa99ae07925f37cb71dc3f
jyp90 <jypark@estatecloudinc.com>
2023-03-19 10:40:12 +0900

    feat: ScaffoldMessenger 를 사용해서 SnackBar 가 화면이동을 하여도 보여지도록 처리하기

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'AppBar',
       theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -17,20 +18,82 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Container(
-          color: Colors.red,
-          width: 100,
-          height: 100,
-          margin: EdgeInsets.symmetric(
-            vertical: 80,
-            horizontal: 20
+        backgroundColor: Colors.blue,
+        body: HomeBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Like a new Snack bar!'),
+              duration: Duration(seconds: 5),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ThirdPage()));
+                },
+              ),
+            ));
+          },
+          child: Icon(Icons.thumb_up),
+        ));
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: ElevatedButton(
+      child: Text('Go to second Page'),
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SecondPage()));
+      },
+    ));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+      body: Center(
+          child: Text(
+        '"좋아요"가 추가되었습니다.',
+        style: TextStyle(fontSize: 20.0, color: Colors.blue),
+      )),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Page"),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '"좋아요"를 취소하시겠습니까?',
+            style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
           ),
-          padding: EdgeInsets.all(40),
-          child: Text('Hello'),
-        ),
-      )
+          ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('"좋아요"가 취소되었습니다.'),
+                  duration: Duration(seconds: 3),
+                ));
+              },
+              child: Text('취소하기')),
+        ],
+      )),
     );
   }
 }
