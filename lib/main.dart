a3a983ddddc3c23f999d1f68ef3f2eb179d83b15
jyp90 <jypark@estatecloudinc.com>
2023-03-19 09:25:04 +0900

    feat: Navigator 설정하여 화면 이동하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AppBar',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: MyPage(),
+      title: 'Navigator',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: const MyPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Container(
-          color: Colors.red,
-          width: 100,
-          height: 100,
-          margin: EdgeInsets.symmetric(
-            vertical: 80,
-            horizontal: 20
-          ),
-          padding: EdgeInsets.all(40),
-          child: Text('Hello'),
+      appBar: AppBar(
+          title: Text('First Page')
+      ),
+      body: Center(
+        child: ElevatedButton(
+            child: Text('Go to the Second Page'),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (ctx) => SecondPage()
+              )); // context 의 위치를 정확히 알아야 stack 으로 쌓는 것이 가능하기 떄문임
+            }
         ),
-      )
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Second page'),
+        ),
+        body: Center(
+            child: ElevatedButton(
+              child: Text('Go to first page'),
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+            )
+        )
     );
   }
 }
