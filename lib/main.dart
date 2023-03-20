e3c3bcfea0ced1e03c2220e1de3636fb5cedeace
jyp90 <jypark@estatecloudinc.com>
2023-03-20 14:09:29 +0900

    feat: Stateful widget, State 상속에 대한 내용

--- lib/main.dart
+++ lib/main.dart
@@ -2,43 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AppBar',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: MyPage(),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class MyPage extends StatelessWidget {
+// Widget 은 기본적으로 상속하면 immutable (불변)
+class MyAppState extends State<MyApp> { // 통상 Stateful widget 을 만들때는 stateless 이름 뒤에 State 를 붙임
   int counter = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Center(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('You have pushed the button this many times:'),
               Text('$counter',
-              style: Theme.of(context).textTheme.displaySmall,)
+                style: Theme.of(context).textTheme.displaySmall,),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: (){
+                      setState(() {
+                        counter++;
+                      });
+                    },
+                  ),
+                  FloatingActionButton(
+                    child: Icon(Icons.remove),
+                    onPressed: (){
+                      setState(() {
+                        counter--;
+                      });
+                    },
+                  )
+                ],
+              )
             ],
           ),
         ),
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          counter++;
-          print("$counter");
-        },
       ),
     );
   }
