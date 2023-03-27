d6503716f066e6225ede16b05a9173f660eed38f
jyp90 <jypark@estatecloudinc.com>
2023-03-27 18:08:20 +0900

    feat: Fetch, async, await 로 api 호출하기 예제

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
-import 'pages/login.dart';
+import 'package:http/http.dart' as http;
+import 'info.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  late final Future<Info> info;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login page',
-      home: LogIn(),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Login page',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Fetch Test'),
+              centerTitle: true,
+            ),
+            body: Center(
+                child: FutureBuilder<Info>(
+                  future: info,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('고객번호: ' + snapshot.data!.id.toString(),
+                            style: TextStyle(fontSize: 20.0),),
+                          Text('고객명: ' + snapshot.data!.userName.toString(),
+                            style: TextStyle(fontSize: 20.0),),
+                          Text('계좌 번호: ' + snapshot.data!.account.toString(),
+                            style: TextStyle(fontSize: 20.0),),
+                          Text('잔액: ' + snapshot.data!.balance.toString(),
+                            style: TextStyle(fontSize: 20.0),)
+                        ],
+                      );
+                    } else if(snapshot.hasError) {
+                      return Text("${snapshot.error}");
+                    }
+                    return CircularProgressIndicator();
+                  },
+
+                )
+            )
+        )
     );
+  }
+}
+
+Future<Info> fetchInfo() async {
+  final response = await http.get(Uri.parse('http://localhost:8080/bank.json?key=1234'));
+
+  if (response.statusCode == 200) {
+    return Info.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('통신 에러가 발생하였습니다.');
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+void showData() async {
+  startTask();
+  String account = await accessData();
+  fetchData(account);
+}
 
-import 'package:flutrain/main.dart';
+void startTask() {
+  print('start task');
+}
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+Future<String> accessData() async {
+  String account = '';
+  Duration time = Duration(seconds: 3);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  if (time.inSeconds > 2) {
+    await Future.delayed(time, ()
+    {
+      account = '8,500';
+      print(account);
+    });
+  } else {
+    String info2 = 'data loaded';
+    print(info2);
+  }
+  return account;
+}
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+void fetchData(String account) {
+  String info3 = 'amount : $account';
+  print(info3);
 }
